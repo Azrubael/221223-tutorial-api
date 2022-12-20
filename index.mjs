04514687b7eb95a86d3bc1e45ef231767a14222c
Azrubael <i0638464000@gmail.com>
2022-12-20 22:30:04 +0200

    Lesson #048. Simple routing with callbacks

--- index.mjs
+++ index.mjs
@@ -6,9 +6,25 @@ import express from 'express'
 const port = 9001
 const app = express()
 
-app.get('/hello', (req, res) => {
+// для этого middlware важен порядок вызова, т.к.
+// обработка может произойти только в случае, когда он первый
+app.all('/hello', (req, res, next) => {
+  console.log('Вызван обработчик "all"')
+  next()
+})
+
+// дополнительный коллбэк обработчик перед обработкой роутов
+// порядок вызова - см. ниже
+const cb = (req, res, next) => {
+   console.log('Callback #1')
+   next()
+}
+
+// коллбэки могут переаваться массивом
+app.get('/hello', cb, (req, res) => {
    res.send('Запуск сервера завершен успешно')
 })
+
 
 app.listen(port, () => {
    console.log(`Сервер запущен на http://localhost:${port}`)
