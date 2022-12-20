f275feea83e7aba332bd77d06e4f1fb42651681f
Azrubael <i0638464000@gmail.com>
2022-12-20 21:53:40 +0200

    Lesson #046. Simple server with switch|case example

--- index.mjs
+++ index.mjs
@@ -4,9 +4,19 @@ const host = 'localhost'
 const port = 9001
 
 const server = http.createServer((req, res) => {
-   res.statusCode = 200
-   res.setHeader('Content-Type', 'text/plain')
-   res.end('Запуск сервера завершен успешно')
+   // сложная логика потребует ветвления switch/case
+   // поэтому в любом случае требуется дополнительная абстракция
+   switch (req.method) {
+      case 'GET':
+         switch (req.url) {
+            case '/hello':
+               res.statusCode = 200
+               res.setHeader('Content-Type', 'text/plain')
+               res.end('Запуск сервера завершен успешно')
+               break               
+         }
+         break
+   }
 })
 
 server.listen(port, host, () => {
