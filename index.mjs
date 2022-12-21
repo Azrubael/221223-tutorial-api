28287e724f0821f14650c3ac794a9b70ba295099
Azrubael <i0638464000@gmail.com>
2022-12-21 12:53:18 +0200

    Lesson #050. Simple router

--- index.mjs
+++ index.mjs
@@ -1,14 +1,17 @@
-// сложная логика потребует ветвления switch/case
-// поэтому в любом случае требуется дополнительная абстракция
-// import http from 'http'
 import express from 'express'
+import { userRouter } from './users/users.mjs'
 
 const port = 9001
 const app = express()
 
 app.get('/hello', (req, res) => {
+   res.set('Content-Type', 'text/plain') // Формат ответа
    res.send('Запуск сервера завершен успешно')
+   res.end()        // Флаг отправки ответа, чтобы сервер не завис
 })
+
+// вызов обработчика userRouter при обращении по адресу '/users'+...
+app.use('/users', userRouter)
 
 app.listen(port, () => {
    console.log(`Сервер запущен на http://localhost:${port}`)
