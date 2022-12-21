7c1ecf73bdb66ab97d6c4783b740d7c66d144fe3
Azrubael <i0638464000@gmail.com>
2022-12-21 20:25:46 +0200

    Lesson #053. Switching to TypeScript. All checked and well tuned

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-import express from 'express'
-import { userRouter } from './users/users.mjs'
+import express, { Request, Response, NextFunction } from 'express'
+import { userRouter } from './users/users'
 
 const port = 9001
 const app = express()
 
-app.get('/hello', (req, res) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
+   console.log('Текущее время  ', Date.now())
+   next()
+})
+
+app.get('/hello', (req: Request, res: Response) => {
    res.set('Content-Type', 'text/plain') // Формат ответа
    res.send('Запуск сервера завершен успешно')
    res.end()        // Флаг отправки ответа, чтобы сервер не завис
@@ -12,6 +17,11 @@ app.get('/hello', (req, res) => {
 
 // вызов обработчика userRouter при обращении по адресу '/users'+...
 app.use('/users', userRouter)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+   console.log(err.message)
+   res.status(401).send(err.message)
+})
 
 app.listen(port, () => {
    console.log(`Сервер запущен на http://localhost:${port}`)
